490e6bf348930f64d7be6056c797ff496d11fecf
warmflasha <warmflasha@gmail.com>
2017-11-16 12:32:39 -0600

    Wrote inclass22

---
+++ Inclass22.m
@@ -0,0 +1,10 @@
+%Inclass 22
+
+%1. Consider the case of the auto-activating gene that we discussed in class
+%today. Make a bifurcation diagram for this system by varying the
+%activated transcription rate for three cases - in which 1, 4, or 8 copies of the
+%transcripton factor are necessary to activate transciption. Comment on your
+%results. 
+
+% 2. Make a similar diagram for the case of an autorepressing gene in the
+% case that 2 copies are need to turn off the gene. 
